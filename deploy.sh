f25a1ac9e96a32b52a010c9fa2779b04995d022e
Lin Pei Wen <mobilesuit0922@gmail.com>
2022-03-12 22:13:35 +0800

    feat: 更新 deploy.sh push 資訊

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m "deploy"
 
-git push -f https://github.com/BardKidd/canBook.git master:gh-pages
+git push -f https://github.com/BardKidd/other.git master:gh-pages
 
 cd -
